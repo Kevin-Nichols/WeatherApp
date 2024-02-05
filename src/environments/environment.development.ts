0a40b8fced6c4c4d364c07cbffd8c8794614bc29
Kevin Nichols <kevnichols2@gmail.com>
2024-02-05 15:32:30 -0500

    added env folder

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherAPIBaseURL: 'https://open-weather13.p.rapidapi.com',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'b0a894654cmshd0da71a746c52c9p14c3c5jsn3dabd51a52d9'
+};
